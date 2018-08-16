94650c5ecae314d8d8f764d5c316e58f6c23e8de
wangzhenjun <wangzhenjun@okay.cn>
2018-08-16 16:00:47 +0800

    更新代码

---
+++ study-jdk/src/main/java/ThreadMain.java
@@ -0,0 +1,57 @@
+public class ThreadMain {
+
+    public static void main(String[] args) {
+//        new ThreadMain().testNew();
+//        new ThreadMain().testRunnable();
+        new ThreadMain().testBlocked();
+    }
+
+    public void testNew() {
+        Thread t = new Thread();
+        System.out.println(t.getState().name());
+    }
+
+    public void testRunnable() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                    System.out.println(i);
+                }
+            }
+        }, "RUNNABLE-Thread");
+        thread.start();
+    }
+
+    public void testBlocked() {
+        final Object lock = new Object();
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    System.out.println(Thread.currentThread().getName() + " invoke");
+                    try {
+                        Thread.sleep(20000l);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "BLOCKED-Thread-A");
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    System.out.println(Thread.currentThread().getName() + " invoke");
+                    try {
+                        Thread.sleep(20000l);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "BLOCKED-Thread-B");
+        threadA.start();
+        threadB.start();
+    }
+}
