7c08d24e30b166c8a46c026a69c5c013501b971c
wangzhenjun <wangzhenjun@okay.cn>
2018-08-16 18:03:03 +0800

    更新代码

--- study-jdk/src/test/java/study/jdk/ThreadMain.java
+++ study-jdk/src/test/java/study/jdk/ThreadMain.java
@@ -1,8 +1,10 @@
+package study.jdk;
+
 public class ThreadMain {
 
     public static void main(String[] args) {
-//        new ThreadMain().testNew();
-//        new ThreadMain().testRunnable();
+//        new study.jdk.ThreadMain().testNew();
+//        new study.jdk.ThreadMain().testRunnable();
         new ThreadMain().testBlocked();
     }
 
---
+++ study-jdk/src/test/java/study/jdk/jcf/QueueTest.java
@@ -0,0 +1,13 @@
+package study.jdk.jcf;
+
+import org.junit.Test;
+
+import java.util.Queue;
+
+public class QueueTest {
+
+    @Test
+    public void test(){
+        Queue queue;
+    }
+}
