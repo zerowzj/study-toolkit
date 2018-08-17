ef86e2b91e7b0f87533b5099a23842727cc3e566
wangzhenjun <wangzhenjun@okay.cn>
2018-08-17 17:22:51 +0800

    更新代码

---
+++ study-jdk/src/test/java/test/NULL.java
@@ -0,0 +1,12 @@
+package test;
+
+public class NULL {
+
+    public void haha() {
+        System.out.println("haha");
+    }
+
+    public static void main(String[] args) {
+        ((NULL) null).haha();
+    }
+}
