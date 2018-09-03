7bb4f606b04f4dc07d9a991ca472f8e9c4faf933
wangzhenjun <wangzhenjun@okay.cn>
2018-09-03 13:28:15 +0800

    更新代码

---
+++ study-java/src/test/java/LocalHost.java
@@ -0,0 +1,26 @@
+import java.net.InetAddress;
+
+public class LocalHost {
+    private static String hostName = getLocalName();
+
+    public static void main(String[] args) {
+        System.out.println("================");
+        try {
+            System.out.println("hostName=" + hostName);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("================");
+    }
+
+    public static String getLocalName() {
+        String hostName;
+        try {
+            InetAddress addr = InetAddress.getLocalHost();
+            hostName = addr.getHostName();
+        } catch (Exception ex) {
+            hostName = "";
+        }
+        return hostName;
+    }
+}
