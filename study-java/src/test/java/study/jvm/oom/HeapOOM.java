e7a51ae93894c0817fb88c9b713d1b9497f17c2f
wangzhenjun <wangzhenjun@okay.cn>
2018-09-05 09:39:12 +0800

    更新代码

--- study-java/src/test/java/study/jvm/oom/HeapOOM.java
+++ study-java/src/test/java/study/jvm/oom/HeapOOM.java
@@ -1,4 +1,21 @@
 package study.jvm.oom;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * -Xms10M -Xmx10M -XX:+HeapDumpOnOutOfMemoryError
+ */
 public class HeapOOM {
+
+    static class OOMObject {
+    }
+
+    static List<OOMObject> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        while(true){
+            list.add(new OOMObject());
+        }
+    }
 }
---
+++ study-java/src/test/java/study/jvm/oom/JavaVMStackSOF.java
@@ -0,0 +1,24 @@
+package study.jvm.oom;
+
+/**
+ * -Xss128K
+ */
+public class JavaVMStackSOF {
+
+    int stackLength = 1;
+
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        }catch (Exception ex){
+            System.out.println("stack length" + oom.stackLength);
+            throw ex;
+        }
+    }
+}
