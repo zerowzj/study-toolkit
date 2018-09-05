4fd5461bdfabe447235df6f7aba04a37f700c71c
wangzhenjun <wangzhenjun@okay.cn>
2018-09-05 13:57:18 +0800

    更新代码

---
+++ study-java/src/test/java/study/jvm/oom/JavaVMStackOOM.java
@@ -0,0 +1,29 @@
+package study.jvm.oom;
+
+/**
+ * -Xss2M
+ */
+public class JavaVMStackOOM {
+
+    public void dontStop(){
+        while(true) {
+        }
+    }
+
+    public void stackLeakByThread(){
+        while(true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
