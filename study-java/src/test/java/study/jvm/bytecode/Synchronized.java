206e67739a013f03b91f12dd9704f2c5ec57f96b
wangzhenjun <wangzhenjun@okay.cn>
2018-09-07 09:25:25 +0800

    更新代码

--- study-java/src/test/java/study/jvm/bytecode/Synchronized.java
+++ study-java/src/test/java/study/jvm/bytecode/Synchronized.java
@@ -4,12 +4,10 @@ public class Synchronized {
 
     public static void main(String[] args) {
         synchronized (Synchronized.class) {
-
         }
         m();
     }
 
     public static synchronized void m() {
-
     }
 }
