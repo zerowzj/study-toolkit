e20151289612bfb10cb006d81ce5b43b579df0cb
wangzhenjun <wangzhenjun@okay.cn>
2018-09-07 09:17:56 +0800

    更新代码

---
+++ study-java/src/test/java/study/jvm/bytecode/Synchronized.java
@@ -0,0 +1,15 @@
+package study.jvm.bytecode;
+
+public class Synchronized {
+
+    public static void main(String[] args) {
+        synchronized (Synchronized.class) {
+
+        }
+        m();
+    }
+
+    public static synchronized void m() {
+
+    }
+}
