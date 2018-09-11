1e4293ed430608f78975f3e908fbb9e901dd195e
wangzhenjun <wangzhenjun@okay.cn>
2018-09-11 15:21:21 +0800

    更新代码

--- study-java/src/test/java/study/jdk/jcf/HashMapTest.java
+++ study-java/src/test/java/study/jdk/jcf/HashMapTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class HashMapTest {
 
     @Test
-    public void sort_test() {
+    public void move_test() {
         //v << n, v左移n
         System.out.println(1 << 0); //1
         System.out.println(1 << 1); //2
@@ -16,6 +16,27 @@ public class HashMapTest {
         System.out.println(4 >> 2); //1
         System.out.println(4 >> 3); //0
         System.out.println(4 >> 5); //0
+    }
+
+    /**
+     * 只有两个操作数对应位同为1时，结果为1，其余全为0.
+     * （或者是只要有一个操作数为0，结果就为0）。
+     */
+    @Test
+    public void bit_and_test() {
+        System.out.println(0 & 0);
+    }
+    /**
+     * 只有两个操作数对应位同为0时，结果为0，其余全为1.（或者是只要有一个操作数为1，结果就为1）。
+     */
+    @Test
+    public void bit_or_test() {
+        System.out.println(0 & 0);
+    }
+
+    @Test
+    public void bit_not_test() {
+        System.out.println(0 & 0);
     }
 
     @Test
