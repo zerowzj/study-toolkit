a2b2cb761cec9be645a3808474a363c0f59b45d8
wangzhenjun <wangzhenjun@okay.cn>
2018-09-12 09:44:52 +0800

    更新代码

--- study-java/src/test/java/study/jdk/concurrent/lock/LockTest.java
+++ study-java/src/test/java/study/jdk/concurrent/lock/LockTest.java
@@ -1,9 +1,13 @@
 package study.jdk.concurrent.lock;
 
+import study.Logger;
+import study.jdk.concurrent.SleepUtils;
+
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class LockTest {
+public class LockTest implements Runnable {
 
     private static Lock lock = new ReentrantLock();
 
@@ -13,8 +17,8 @@ public class LockTest {
 //                    .start();
 //        }
 
-        Thread t1 = new Thread(new LockTask(lock));
-        Thread t2 = new Thread(new LockTask(lock));
+        Thread t1 = new Thread(new LockTest());
+        Thread t2 = new Thread(new LockTest());
         t1.start();
         t2.start();
         try {
@@ -23,5 +27,75 @@ public class LockTest {
             ex.printStackTrace();
         }
         t2.interrupt();
+    }
+
+    @Override
+    public void run() {
+//        lock();
+//        tryLock();
+        lockInterruptibly();
+    }
+
+    /**
+     *
+     */
+    public void lock() {
+        lock.lock();
+        try {
+            Logger.info(String.format("[%s]得到了锁", Thread.currentThread().getName()));
+            TimeUnit.SECONDS.sleep(5);
+        } catch (Exception ex) {
+
+        } finally {
+            Logger.info(String.format("[%s]释放了锁", Thread.currentThread().getName()));
+            lock.unlock();
+        }
+    }
+
+    /**
+     *
+     */
+    public void tryLock() {
+        if (lock.tryLock()) {
+            try {
+                Logger.info(String.format("[%s]得到了锁", Thread.currentThread().getName()));
+                SleepUtils.second(5);
+            } finally {
+                Logger.info(String.format("[%s]释放了锁", Thread.currentThread().getName()));
+                lock.unlock();
+            }
+        } else {
+            Logger.info(String.format("[%s]获取锁失败", Thread.currentThread().getName()));
+        }
+    }
+
+    /**
+     *
+     */
+    public void tryLock(long time) {
+
+    }
+
+    /**
+     *
+     */
+    public void lockInterruptibly() {
+        //注意，如果需要正确中断等待锁的线程，必须将获取锁放在外面，然后将InterruptedException抛出
+        try {
+            lock.lockInterruptibly();
+
+            Logger.info(String.format("[%s]得到了锁", Thread.currentThread().getName()));
+            long startTime = System.currentTimeMillis();
+            for (; ; ) {
+                if (System.currentTimeMillis() - startTime >= Integer.MAX_VALUE)
+                    break;
+            }
+        } catch (InterruptedException ex) {
+
+        } finally {
+            Logger.info(String.format("[%s]执行finally", Thread.currentThread().getName()));
+            lock.unlock();
+            Logger.info(String.format("[%s]释放了锁", Thread.currentThread().getName()));
+        }
     }
 }
