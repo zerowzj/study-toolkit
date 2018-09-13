c1f045f0f764e2728bf85591d448c30f8ec1aa18
wangzhenjun <wangzhenjun@okay.cn>
2018-09-13 18:00:55 +0800

    更新代码

--- study-java/src/test/java/study/jdk/concurrent/lock/ReadWriteLockTest.java
+++ study-java/src/test/java/study/jdk/concurrent/lock/ReadWriteLockTest.java
@@ -1,8 +1,6 @@
 package study.jdk.concurrent.lock;
 
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteLockTest {
