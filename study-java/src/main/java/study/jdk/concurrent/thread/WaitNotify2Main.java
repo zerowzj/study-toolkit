1e9e1416bcf409742a89401313f341c53578d67f
wangzhenjun <wangzhenjun@okay.cn>
2018-10-09 15:41:17 +0800

    更新代码

--- study-java/src/main/java/study/jdk/concurrent/thread/WaitNotify2Main.java
+++ study-java/src/main/java/study/jdk/concurrent/thread/WaitNotify2Main.java
@@ -1,9 +1,28 @@
 package study.jdk.concurrent.thread;
 
 public class WaitNotify2Main {
-    static Object lock = new Object();
+
+    private static int sum = 0;
 
     public static void main(String[] args) {
 
+        Object lock = new Object();
+
+        Thread t1 = new Thread(() -> {
+            synchronized (lock) {
+                for (; ; ) {
+                    sum++;
+                    if (sum == 1000) {
+                        lock.notify();
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println(sum);
+        });
+        t1.start();
+        t2.start();
     }
 }
