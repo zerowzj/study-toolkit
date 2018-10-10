50e5ef813ff14f3f0a21a891c4a0099f7473ca0a
wangzhenjun <wangzhenjun@okay.cn>
2018-10-10 13:55:56 +0800

    更新代码

--- study-java/src/main/java/study/jdk/concurrent/thread/Join1Main.java
+++ study-java/src/main/java/study/jdk/concurrent/thread/Join1Main.java
@@ -5,7 +5,16 @@ import study.Sleeps;
 public class Join1Main {
 
     public static void main(String[] args) {
+        Join1Main main = new Join1Main();
+        main.test1();
+        main.test2();
+        System.out.println("i am main thread");
+    }
 
+    /**
+     * 演示join原理
+     */
+    public void test1() {
         Thread t = new Thread(() -> {
             Sleeps.seconds(2);
             System.out.println("i am t thread");
@@ -13,10 +22,31 @@ public class Join1Main {
         t.start();
         try {
             t.join();
+
+//            synchronized (t) {
+//                t.wait();
+//            }
+
         } catch (InterruptedException ex) {
-
         }
+    }
 
-        System.out.println("i am main thread");
+    /**
+     * 演示只阻塞父线程
+     */
+    public void test2() {
+        Thread t1 = new Thread(() -> {
+            Sleeps.seconds(2);
+            System.out.println("i am t1 thread");
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                t1.join();
+            } catch (InterruptedException ex) {
+            }
+            System.out.println("i am t2 thread");
+        });
+        t1.start();
+        t2.start();
     }
 }
