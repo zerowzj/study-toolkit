2c632cf7c816dfe456ac70734ecced7da0bf0c62
wangzhenjun <wangzhenjun@okay.cn>
2018-10-12 17:31:41 +0800

    更新代码

--- study-java/src/main/java/study/jdk/concurrent/thread/Callable1_Main.java
+++ study-java/src/main/java/study/jdk/concurrent/thread/Callable1_Main.java
@@ -4,21 +4,27 @@ import study.Loggers;
 import study.Sleeps;
 
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
 
 public class Callable1_Main {
 
     public static void main(String[] args) {
-        FutureTask<Integer> task = new FutureTask(() -> {
+        FutureTask<Integer> task1 = new FutureTask(() -> {
             int sum = 10;
             Sleeps.seconds(3);
-            Loggers.info("i am task");
+            Loggers.info("i am task1");
             return sum;
         });
-        Thread t = new Thread(task);
-        t.start();
+        FutureTask<Integer> task2 = new FutureTask(() -> {
+            int sum = 21;
+            Loggers.info("i am task2");
+            return sum;
+        });
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+        t1.start();
+        t2.start();
         try {
-            Integer sum = task.get(4, TimeUnit.SECONDS);
+            Integer sum = task1.get() + task2.get();
             Loggers.info("sum= {}", sum);
         } catch (Exception ex) {
             ex.printStackTrace();
