bef34eae07908104798018c23c0b6706b969a510
wangzhenjun <wangzhenjun@okay.cn>
2018-10-16 14:33:08 +0800

    更新代码

---
+++ study-java/src/main/java/study/jdk/concurrent/pool/ThreadPoolExecutor_Main.java
@@ -0,0 +1,24 @@
+package study.jdk.concurrent.pool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutor_Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThreadPoolExecutor_Main.class);
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 10, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100));
+
+        for (int i = 0; i < 5; i++) {
+            final int j = i;
+            pool.execute(() -> {
+                LOGGER.info("i am thread[{}]", j + 1);
+            });
+        }
+    }
+}
