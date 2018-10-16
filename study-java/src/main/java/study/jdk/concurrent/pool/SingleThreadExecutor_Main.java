7f904831470313320557d7d5325791ed6cfa55fb
wangzhenjun <wangzhenjun@okay.cn>
2018-10-16 15:31:09 +0800

    更新代码

---
+++ study-java/src/main/java/study/jdk/concurrent/pool/SingleThreadExecutor_Main.java
@@ -0,0 +1,28 @@
+package study.jdk.concurrent.pool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import study.Sleeps;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ */
+public class SingleThreadExecutor_Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SingleThreadExecutor_Main.class);
+
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++) {
+            final int j = i;
+            pool.execute(() -> {
+                LOGGER.info("i am thread[{}]", j + 1);
+                Sleeps.seconds(20000);
+            });
+        }
+        pool.shutdown();
+    }
+}
