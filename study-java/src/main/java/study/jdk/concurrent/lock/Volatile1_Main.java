b6dbb6ff70f019b920b9ab2c6b98990d70db1a68
wangzhenjun <wangzhenjun@okay.cn>
2018-10-18 10:44:12 +0800

    更新代码

--- study-java/src/main/java/study/jdk/concurrent/lock/Volatile1_Main.java
+++ study-java/src/main/java/study/jdk/concurrent/lock/Volatile1_Main.java
@@ -2,30 +2,46 @@ package study.jdk.concurrent.lock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import study.Sleeps;
 
+import java.util.concurrent.CountDownLatch;
+
+/**
+ *
+ */
 public class Volatile1_Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Volatile1_Main.class);
 
-    private volatile static int count = 0;
+    private volatile int count = 0;
 
     public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+           new Volatile1_Main().show();
+        }
+    }
 
+    private void show() {
+        CountDownLatch latch = new CountDownLatch(2);
         Thread t1 = new Thread(() -> {
             addCount();
+            latch.countDown();
         });
         Thread t2 = new Thread(() -> {
             addCount();
+            latch.countDown();
         });
         t1.start();
         t2.start();
-        Sleeps.seconds(5);
 
+        try {
+            latch.await();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         LOGGER.info("count= {}", count);
     }
 
-    private static void addCount() {
+    private synchronized void addCount() {
         for (int i = 0; i < 1000; i++) {
             count++;
         }
