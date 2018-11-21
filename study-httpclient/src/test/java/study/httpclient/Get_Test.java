f7f1ecad9e3a1ab3d07e31f6d12e8eba56e49b1c
wangzhenjun <wangzhenjun@okay.cn>
2018-11-21 17:34:17 +0800

    更新代码

--- study-httpclient/src/test/java/study/httpclient/Get_Test.java
+++ study-httpclient/src/test/java/study/httpclient/Get_Test.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-public class Test1 {
+public class Get_Test {
 
     @Test
     public void test() {
---
+++ study-httpclient/src/test/java/study/httpclient/Post_Test.java
@@ -0,0 +1,4 @@
+package study.httpclient;
+
+public class Post_Test {
+}
