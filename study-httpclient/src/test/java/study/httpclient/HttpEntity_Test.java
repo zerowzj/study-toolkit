9b9b34a94ff68a7a5c9a51a580e36d4212460372
wangzhenjun <wangzhenjun@okay.cn>
2018-11-23 14:02:23 +0800

    更新代码

--- study-httpclient/src/test/java/study/httpclient/HttpEntity_Test.java
+++ study-httpclient/src/test/java/study/httpclient/HttpEntity_Test.java
@@ -1,6 +1,56 @@
 package study.httpclient;
 
-import study.httpclient.HttpClient_Test;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
+
+import java.io.IOException;
 
 public class HttpEntity_Test extends HttpClient_Test {
+
+    @Test
+    public void test_StringEntity() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost();
+        try {
+            //
+            StringEntity entity = new StringEntity("");
+            httpPost.setEntity(entity);
+
+            HttpResponse response = httpClient.execute(httpPost);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void test_UrlEncodedFormEntity() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost();
+        try {
+            //
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(null);
+            httpPost.setEntity(entity);
+
+            HttpResponse response = httpClient.execute(httpPost);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
