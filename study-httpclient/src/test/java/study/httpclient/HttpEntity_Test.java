e18a461c3577a6cbed066304987ced4640f3ffd7
wangzhenjun <wangzhenjun@okay.cn>
2018-11-26 15:16:10 +0800

    更新代码

--- study-httpclient/src/test/java/study/httpclient/HttpEntity_Test.java
+++ study-httpclient/src/test/java/study/httpclient/HttpEntity_Test.java
@@ -3,6 +3,7 @@ package study.httpclient;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -12,45 +13,59 @@ import java.io.IOException;
 
 public class HttpEntity_Test extends HttpClient_Test {
 
+    /**
+     *
+     */
     @Test
     public void test_StringEntity() {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost();
         try {
-            //
+            //TODO
             StringEntity entity = new StringEntity("");
+
+            HttpPost httpPost = new HttpPost();
             httpPost.setEntity(entity);
 
             HttpResponse response = httpClient.execute(httpPost);
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
-            try {
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
+    /**
+     *
+     */
     @Test
     public void test_UrlEncodedFormEntity() {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost();
         try {
-            //
+            //TODO
             UrlEncodedFormEntity entity = new UrlEncodedFormEntity(null);
+
+            HttpPost httpPost = new HttpPost();
             httpPost.setEntity(entity);
 
             HttpResponse response = httpClient.execute(httpPost);
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
-            try {
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
+
+
 }
---
+++ study-httpclient/src/test/java/study/httpclient/MultipartEntity_Test.java
@@ -0,0 +1,43 @@
+package study.httpclient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MultipartEntity_Test extends HttpClient_Test {
+
+    /**
+     *
+     */
+    @Test
+    public void test() {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+//        builder.addBinaryBody()
+//        builder.addTextBody()
+        HttpEntity entity = builder.build();
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            HttpPost httpPost = new HttpPost();
+            httpPost.setEntity(entity);
+
+            HttpResponse response = httpClient.execute(httpPost);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
