646aee308391dad073fb03b2ef8453aa3bf15d61
wangzhenjun <wangzhenjun@okay.cn>
2018-11-26 17:53:06 +0800

    更新代码

--- study-httpclient/src/main/java/com/company/project/HttpClientUtil.java
+++ study-httpclient/src/main/java/com/company/project/HttpClientUtil.java
@@ -1,22 +1,38 @@
 package com.company.project;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class HttpClientUtil {
 
-    public static void form(String url, Map<String, Object> params) {
+    /**
+     * 表单提交请求
+     *
+     * @param url
+     * @param params
+     */
+    public static void form(String url, Map<String, String> params) {
         HttpClient client = HttpClients.createDefault();
         HttpPost post = new HttpPost(url);
         try {
-            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(null, "UTF-8");
-            post.setEntity(entity);
+            if (params != null) {
+                List<NameValuePair> pairLt = new ArrayList<>();
+                for (Map.Entry<String, String> param : params.entrySet()) {
+                    pairLt.add(new BasicNameValuePair(param.getKey(), param.getValue()));
+                }
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(pairLt, "UTF-8");
+                post.setEntity(entity);
+            }
             HttpResponse response = client.execute(post);
 
             response.getEntity();
@@ -24,6 +40,12 @@ public class HttpClientUtil {
         }
     }
 
+    /**
+     * 表单提交请求
+     *
+     * @param url
+     * @param params
+     */
     public static void post(String url, Map<String, Object> params) {
         HttpClient client = HttpClients.createDefault();
         HttpPost post = new HttpPost(url);
