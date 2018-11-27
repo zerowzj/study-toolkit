1a5201471fbf43dcc7eec7fdb3db1bb71771b043
wangzhenjun <wangzhenjun@okay.cn>
2018-11-27 14:01:16 +0800

    更新代码

---
+++ study-httpclient/src/main/java/com/company/project/HttpClientUtilx.java
@@ -0,0 +1,26 @@
+package com.company.project;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpClientUtilx {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientUtilx.class);
+
+    private static final String CHARSET_UTF_8 = "UTF-8";
+}
