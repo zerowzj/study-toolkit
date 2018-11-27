751874e100d3bbaff1b3747b89c315657fc2ceb1
wangzhenjun <wangzhenjun@okay.cn>
2018-11-27 15:29:41 +0800

    更新代码

--- study-httpclient/src/main/java/com/company/project/HttpClientUtilx.java
+++ study-httpclient/src/main/java/com/company/project/HttpClientUtilx.java
@@ -1,22 +1,7 @@
 package com.company.project;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class HttpClientUtilx {
 
