6c0c31bd3350718afc80d990664518407ea2493e
wangzhenjun <wangzhenjun@okay.cn>
2018-11-28 14:34:26 +0800

    更新代码

--- study-httpclient/src/test/java/study/httpclient/method/Post_Test.java
+++ study-httpclient/src/test/java/study/httpclient/method/Post_Test.java
@@ -13,6 +13,7 @@ public class Post_Test extends HttpClient_Test {
     @Test
     public void test() {
         HttpPost httpPost = new HttpPost(URL);
+
         try {
             CloseableHttpResponse response = client.execute(httpPost);
             //
