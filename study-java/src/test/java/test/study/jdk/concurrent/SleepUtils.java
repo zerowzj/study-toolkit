c251b6a2aecce76b35e19b8ccdb80dcf03e228ee
wangzhenjun <wangzhenjun@okay.cn>
2019-03-13 14:39:38 +0800

    更新代码

--- study-java/src/test/java/test/study/jdk/concurrent/SleepUtils.java
+++ study-java/src/test/java/test/study/jdk/concurrent/SleepUtils.java
@@ -7,16 +7,16 @@ public class SleepUtils {
     public static void seconds(long seconds) {
         try {
             TimeUnit.SECONDS.sleep(seconds);
-        } catch (Exception ex) {
-
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
 
     public static void milliseconds(long milliseconds) {
         try {
             TimeUnit.MILLISECONDS.sleep(milliseconds);
-        } catch (Exception ex) {
-
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
 }
