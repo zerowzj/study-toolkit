30cc10004f146441585d339c3da710ca07fe0de4
wangzhenjun <wangzhenjun@okay.cn>
2019-03-22 09:58:19 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/thread/State3_Main.java
+++ study-java/src/main/java/study/jdk/juc/thread/State3_Main.java
@@ -1,37 +1,39 @@
 package study.jdk.juc.thread;
 
-import study.Sleeps;
-
 /**
- * "main" #1 prio=5 os_prio=0 tid=0x0000000004772800 nid=0xd38 in Object.wait() [0x00000000045af000]
+ * "t1" #11 prio=5 os_prio=0 tid=0x000000001c03e800 nid=0x2fb0 in Object.wait() [0x000000001c91e000]
  *    java.lang.Thread.State: WAITING (on object monitor)
  *         at java.lang.Object.wait(Native Method)
- *         - waiting on <0x00000000d71495b8> (a java.lang.Thread)
- *         at java.lang.Thread.join(Thread.java:1249)
- *         - locked <0x00000000d71495b8> (a java.lang.Thread)
- *         at java.lang.Thread.join(Thread.java:1323)
- *         at study.jdk.juc.thread.State3_Main.main(State3_Main.java:18)
+ *         - waiting on <0x00000000d71493b0> (a java.lang.Object)
+ *         at java.lang.Object.wait(Object.java:502)
+ *         at study.jdk.juc.thread.State3_Main.lambda$main$0(State3_Main.java:13)
+ *         - locked <0x00000000d71493b0> (a java.lang.Object)
+ *         at study.jdk.juc.thread.State3_Main$$Lambda$1/1607521710.run(Unknown Source)
+ *         at java.lang.Thread.run(Thread.java:745)
  *
- * "main" #1 prio=5 os_prio=0 tid=0x0000000004e32800 nid=0x43ac in Object.wait() [0x0000000004c5f000]
+ * "t1" #11 prio=5 os_prio=0 tid=0x000000001b86d000 nid=0x2344 in Object.wait() [0x000000001c13f000]
  *    java.lang.Thread.State: TIMED_WAITING (on object monitor)
  *         at java.lang.Object.wait(Native Method)
- *         - waiting on <0x00000000d7149900> (a java.lang.Thread)
- *         at java.lang.Thread.join(Thread.java:1257)
- *         - locked <0x00000000d7149900> (a java.lang.Thread)
- *         at study.jdk.juc.thread.State3_Main.main(State3_Main.java:25)
+ *         - waiting on <0x00000000d7149338> (a java.lang.Object)
+ *         at study.jdk.juc.thread.State3_Main.lambda$main$0(State3_Main.java:24)
+ *         - locked <0x00000000d7149338> (a java.lang.Object)
+ *         at study.jdk.juc.thread.State3_Main$$Lambda$1/1607521710.run(Unknown Source)
+ *         at java.lang.Thread.run(Thread.java:745)
  */
 public class State3_Main {
 
     public static void main(String[] args) {
+        Object lock = new Object();
         Thread t = new Thread(() -> {
-            Sleeps.seconds(Integer.MAX_VALUE);
-        }, "ssssssssss");
+            synchronized (lock) {
+                try {
+//                    lock.wait();
+                    lock.wait(Integer.MAX_VALUE);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }, "t1");
         t.start();
-
-        try {
-            t.join(Integer.MAX_VALUE);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
     }
 }
