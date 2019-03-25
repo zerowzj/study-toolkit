46d1206eeefe76968e05bc983584215451f67fc4
wangzhenjun <wangzhenjun@okay.cn>
2019-03-25 15:53:42 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/thread/Callable1_Main.java
+++ study-java/src/main/java/study/jdk/juc/thread/Callable1_Main.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import study.Sleeps;
 
-import java.lang.invoke.MethodHandles;
 import java.util.concurrent.FutureTask;
 
 /**
@@ -12,22 +11,19 @@ import java.util.concurrent.FutureTask;
  */
 public class Callable1_Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final Logger LOGGER = LoggerFactory.getLogger(Callable1_Main.class);
 
     public static void main(String[] args) {
         long now = System.currentTimeMillis();
-
         FutureTask<Integer> task1 = new FutureTask(() -> {
-            int sum = 10;
-            Sleeps.seconds(3); //3秒
+            Sleeps.seconds(3);
             LOGGER.info("i am task1");
-            return sum;
+            return 10;
         });
         FutureTask<Integer> task2 = new FutureTask(() -> {
-            int sum = 21;
-            Sleeps.seconds(5); //5秒
+            Sleeps.seconds(5);
             LOGGER.info("i am task2");
-            return sum;
+            return 21;
         });
         Thread t1 = new Thread(task1);
         Thread t2 = new Thread(task2);
@@ -40,7 +36,6 @@ public class Callable1_Main {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
         LOGGER.info("i am main thread");
     }
 }
