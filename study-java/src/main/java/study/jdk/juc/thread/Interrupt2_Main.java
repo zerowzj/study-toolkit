51f6af15f5015ab9f04dcb85b75e07b96dea9a0e
wangzhenjun <wangzhenjun@okay.cn>
2019-03-26 10:53:56 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/thread/Interrupt2_Main.java
+++ study-java/src/main/java/study/jdk/juc/thread/Interrupt2_Main.java
@@ -14,15 +14,14 @@ public class Interrupt2_Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Interrupt2_Main.class);
 
     public static void main(String[] args) {
-
         Thread t = new Thread(() -> {
-            //
+            //普通方法
 //            while (true) {
 //                //LOGGER.info("loop");
 //            }
-            //
+            //阻塞方法
             try {
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(20);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
