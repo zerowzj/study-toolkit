109f3bb49dac7796429c072d37b470ec58147959
wangzhenjun <wangzhenjun@okay.cn>
2019-03-27 14:36:46 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/lock/Synchronized1_Main.java
+++ study-java/src/main/java/study/jdk/juc/lock/Synchronized1_Main.java
@@ -5,33 +5,32 @@ import org.slf4j.LoggerFactory;
 import study.Sleeps;
 
 /**
- * 不可中断
+ *
  */
 public class Synchronized1_Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Synchronized1_Main.class);
 
-    private Object lock = new Object();
+    synchronized void a() {
+        LOGGER.info("i am aaa");
+        Sleeps.deep();
+    }
 
-    public void test() {
+    synchronized void b() {
+        LOGGER.info("i am bbb");
+    }
+
+    void test() {
         Thread t1 = new Thread(() -> {
-            synchronized (lock) {
-                LOGGER.info("i am thread t1");
-                Sleeps.deep();
-            }
+            a();
         });
         Thread t2 = new Thread(() -> {
-            synchronized (lock) {
-                LOGGER.info("i am thread t2");
-            }
+            b();
         });
+
         t1.start();
         Sleeps.seconds(1);
         t2.start();
-
-        //main线程中断t2
-        Sleeps.seconds(3);
-        t2.interrupt();
     }
 
     public static void main(String[] args) {
