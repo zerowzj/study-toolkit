d0a1d20777bed40ec7fa9cee497ac1046d0b7ebb
wangzhenjun <wangzhenjun@okay.cn>
2019-03-27 15:55:36 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/lock/ReadWriteLock1_Main.java
+++ study-java/src/main/java/study/jdk/juc/lock/ReadWriteLock1_Main.java
@@ -3,6 +3,9 @@ package study.jdk.juc.lock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+ /**
+  * 演示：
+  */
 public class ReadWriteLock1_Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReadWriteLock1_Main.class);
