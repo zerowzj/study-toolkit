023ac11c4d77689fc2fe9cfe5409ec313578f9fc
wangzhenjun <wangzhenjun@okay.cn>
2019-03-28 17:37:25 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/thread/Daemon1_Main.java
+++ study-java/src/main/java/study/jdk/juc/thread/Daemon1_Main.java
@@ -1,4 +1,27 @@
 package study.jdk.juc.thread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import study.Sleeps;
+
+/**
+ * 演示：守护线程
+ */
 public class Daemon1_Main {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Daemon1_Main.class);
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while(true){
+                LOGGER.info("i am thread t, sleep 2s");
+                Sleeps.seconds(2);
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+
+        LOGGER.info("i am thread main, sleep 10s");
+        Sleeps.seconds(10);
+    }
 }
