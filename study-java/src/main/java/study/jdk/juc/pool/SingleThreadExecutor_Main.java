a8ba729dcb626554272e08ff0ca70dcc6f103197
wangzhenjun <wangzhenjun@okay.cn>
2019-04-03 11:33:22 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/pool/SingleThreadExecutor_Main.java
+++ study-java/src/main/java/study/jdk/juc/pool/SingleThreadExecutor_Main.java
@@ -8,7 +8,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 演示：单线程执行器
+ * 演示：
+ * （1）单线程执行器
+ * （2）主线程结束，线程池线程可能还在运行
+ * （3）任务和线程是2个概念
  */
 public class SingleThreadExecutor_Main {
 
@@ -19,13 +22,12 @@ public class SingleThreadExecutor_Main {
     public static void main(String[] args) {
         ExecutorService pool = Executors.newSingleThreadExecutor();
         for (int i = 0; i < TASK_NUM; i++) {
-            int no = i + 1;
+            int taskNo = i + 1;
             pool.execute(() -> {
-                LOGGER.info("i am task[{}], sleep 2s", no);
+                LOGGER.info("i am task[{}], sleep 2s", taskNo);
                 Sleeps.seconds(2);
             });
         }
-        //main线程结束，程序依然运行一会儿
         LOGGER.info("i am main thread");
         pool.shutdown();
         LOGGER.info("main thread end");
