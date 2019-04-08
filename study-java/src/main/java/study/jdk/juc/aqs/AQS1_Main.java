4fbe9b02733d1c4ca984d7274a3347259c9063f4
wangzhenjun <wangzhenjun@okay.cn>
2019-04-08 17:05:27 +0800

    更新代码

--- study-java/src/main/java/study/jdk/juc/aqs/AQS1_Main.java
+++ study-java/src/main/java/study/jdk/juc/aqs/AQS1_Main.java
@@ -6,7 +6,6 @@ import study.Sleeps;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 /**
  * 演示：Unsafe
@@ -18,65 +17,7 @@ public class AQS1_Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AQS1_Main.class);
 
-    /**
-     *
-     */
-    private class Mutex {
-
-        /**
-         * 自定义同步器
-         */
-        class Sync extends AbstractQueuedSynchronizer {
-
-            //是否处于占用状态
-            @Override
-            protected boolean isHeldExclusively() {
-                return getState() == 1;
-            }
-
-            //当状态为0的时候获取锁，CAS操作成功，则state状态为1，
-            @Override
-            protected boolean tryAcquire(int acquires) {
-                if (compareAndSetState(0, 1)) {
-                    setExclusiveOwnerThread(Thread.currentThread());
-                    return true;
-                }
-                return false;
-            }
-
-            //释放锁，将同步状态置为0
-            @Override
-            protected boolean tryRelease(int acquires) {
-                if (getState() == 0)
-                    throw new IllegalMonitorStateException();
-                setExclusiveOwnerThread(null);
-                setState(0);
-                return true;
-            }
-
-            @Override
-            protected int tryAcquireShared(int arg) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            protected boolean tryReleaseShared(int arg) {
-                throw new UnsupportedOperationException();
-            }
-        }
-
-        private Sync sync = new Sync();
-
-        public void lock() {
-            sync.acquire(1);
-        }
-
-        public void unlock() {
-            sync.release(1);
-        }
-    }
-
-    private void test() {
+    public static void main(String[] args) {
         ExecutorService pool = Executors.newFixedThreadPool(3);
         Mutex lock = new Mutex();
         for (int i = 0; i < 3; i++) {
@@ -94,9 +35,5 @@ public class AQS1_Main {
         }
         pool.shutdown();
         LOGGER.info("main thread end");
-    }
-
-    public static void main(String[] args) {
-        new AQS1_Main().test();
     }
 }
