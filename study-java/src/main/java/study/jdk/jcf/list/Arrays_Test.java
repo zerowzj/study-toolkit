3e439b22f857e8071b72905a8031b8ed8d0a76ae
wangzhenjun <wangzhenjun@okay.cn>
2019-04-09 15:06:01 +0800

    更新代码

--- study-java/src/main/java/study/jdk/jcf/list/Arrays_Test.java
+++ study-java/src/main/java/study/jdk/jcf/list/Arrays_Test.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,12 +11,18 @@ public class Arrays_Test {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Arrays_Test.class);
 
+    /**
+     * 演示：
+     */
     @Test
     public void test_asList() {
         List<String> data = Arrays.asList("a", "b");
         LOGGER.info("data={}", data);
     }
 
+    /**
+     * 演示：
+     */
     @Test
     public void test_copyOf() {
         String[] original = new String[]{"a", "b", "c", "d"};
