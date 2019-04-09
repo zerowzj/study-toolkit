b548ed7a0d0980ad49b5015782b49ac668d3ca45
wangzhenjun <wangzhenjun@okay.cn>
2019-04-09 11:06:42 +0800

    更新代码

--- study-java/src/main/java/study/jdk/jcf/list/ArrayList_Test.java
+++ study-java/src/main/java/study/jdk/jcf/list/ArrayList_Test.java
@@ -1,77 +1,69 @@
 package study.jdk.jcf.list;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import study.jdk.jcf.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArrayList_Test {
 
-    private ArrayList<String> lt = new ArrayList();
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArrayList_Test.class);
 
     /**
-     * 输出结果：
-     * i=0
-     * (++i)=1
-     * i=1
-     * a=0
-     * (a++)=0
-     * a=1
+     * 演示：自增自减操作
      */
     @Test
-    public void test() {
-//        int i = 0;
-//        System.out.println("    i=" + i);
-//        System.out.println("(++i)=" + (++i));
-//        System.out.println("    i=" + i);
-//
-//        int a = 0;
-//        System.out.println("    a=" + a);
-//        System.out.println("(a++)=" + (a++));
-//        System.out.println("    a=" + a);
+    public void test_incre_decre() {
+        int a = 0;
+        LOGGER.info("    a=", a);
+        LOGGER.info("(++a)=" + (++a));
+        LOGGER.info("    a=" + a);
 
-        Integer aa = null;
-        Long bb = null;
-        System.out.println(aa == null ? bb : aa);
+        int b = 0;
+        LOGGER.info("    b=" + b);
+        LOGGER.info("(b++)=" + (b++));
+        LOGGER.info("    b=" + b);
     }
 
     /**
-     * 输出结果：
-     * 10
-     * 1
+     * 演示：添加元素
      */
     @Test
-    public void test_math() {
-        System.out.println("max=" + Math.max(10, 1));
-        System.out.println("min=" + Math.min(10, 1));
-    }
-
-    @Test
-    public void add_test() {
+    public void test_add() {
+        List<String> lt = new ArrayList<>();
         lt.add("a");
         lt.add("b");
-        System.out.println(lt);
+        LOGGER.info("{}", lt);
 
         lt.add(0, "1");
-        System.out.println(lt);
+        LOGGER.info("{}", lt);
 
         lt.addAll(Arrays.asList("2", "3"));
-        System.out.println(lt);
+        LOGGER.info("{}", lt);
 
         lt.addAll(0, Arrays.asList("4", "5"));
-        System.out.println(lt);
+        LOGGER.info("{}", lt);
     }
 
+    /**
+     * 演示：获取元素
+     */
     @Test
     public void get_test() {
-        add_test();
+        ArrayList<String> lt = new ArrayList<>();
         System.out.println(lt.get(3));
     }
 
+    /**
+     * 演示：包含元素
+     */
     @Test
     public void contains_test() {
-        add_test();
+        List<String> lt = new ArrayList<>();
         System.out.println(lt.contains("a"));
 
         ArrayList<User> ult = new ArrayList();
