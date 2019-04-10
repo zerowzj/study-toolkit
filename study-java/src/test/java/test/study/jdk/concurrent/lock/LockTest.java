bb1da2912a45ba292cde494f6b4c5f6aba557b09
wangzhenjun <wangzhenjun@okay.cn>
2019-04-10 09:48:51 +0800

    更新代码

--- study-java/src/test/java/test/study/jdk/concurrent/lock/LockTest.java
+++ study-java/src/test/java/test/study/jdk/concurrent/lock/LockTest.java
@@ -1,6 +1,5 @@
 package test.study.jdk.concurrent.lock;
 
-import study.Loggers;
 import test.study.jdk.concurrent.SleepUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -38,12 +37,10 @@ public class LockTest implements Runnable {
     public void lock() {
         lock.lock();
         try {
-            Loggers.info(String.format("[%s]得到了锁", Thread.currentThread().getName()));
             TimeUnit.SECONDS.sleep(5);
         } catch (Exception ex) {
 
         } finally {
-            Loggers.info(String.format("[%s]释放了锁", Thread.currentThread().getName()));
             lock.unlock();
         }
     }
@@ -54,14 +51,14 @@ public class LockTest implements Runnable {
     public void tryLock() {
         if (lock.tryLock()) {
             try {
-                Loggers.info(String.format("[%s]得到了锁", Thread.currentThread().getName()));
+                System.out.println(String.format("[%s]得到了锁", Thread.currentThread().getName()));
                 SleepUtils.seconds(5);
             } finally {
-                Loggers.info(String.format("[%s]释放了锁", Thread.currentThread().getName()));
+                System.out.println(String.format("[%s]释放了锁", Thread.currentThread().getName()));
                 lock.unlock();
             }
         } else {
-            Loggers.info(String.format("[%s]获取锁失败", Thread.currentThread().getName()));
+            System.out.println(String.format("[%s]获取锁失败", Thread.currentThread().getName()));
         }
     }
 
@@ -79,7 +76,7 @@ public class LockTest implements Runnable {
         //注意，如果需要正确中断等待锁的线程，必须将获取锁放在外面，然后将InterruptedException抛出
         try {
             lock.lockInterruptibly();
-            Loggers.info(String.format("[%s]得到了锁", Thread.currentThread().getName()));
+            System.out.println(String.format("[%s]得到了锁", Thread.currentThread().getName()));
             long startTime = System.currentTimeMillis();
             for (; ; ) {
                 if (System.currentTimeMillis() - startTime >= Integer.MAX_VALUE)
@@ -88,9 +85,9 @@ public class LockTest implements Runnable {
         } catch (InterruptedException ex) {
 
         } finally {
-            Loggers.info(String.format("[%s]执行finally", Thread.currentThread().getName()));
+            System.out.println(String.format("[%s]执行finally", Thread.currentThread().getName()));
             lock.unlock();
-            Loggers.info(String.format("[%s]释放了锁", Thread.currentThread().getName()));
+            System.out.println(String.format("[%s]释放了锁", Thread.currentThread().getName()));
         }
     }
 }
