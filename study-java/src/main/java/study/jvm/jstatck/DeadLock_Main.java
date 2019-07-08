f51b91796966770c4a2480aa85634127ca839e18
wangzhenjun <wangzhenjun@okay.cn>
2019-07-08 15:07:04 +0800

    更新代码

---
+++ study-java/src/main/java/study/jvm/jstatck/DeadLock_Main.java
@@ -0,0 +1,31 @@
+package study.jvm.jstatck;
+
+import org.apache.tools.ant.taskdefs.Sleep;
+import study.Sleeps;
+
+/**
+ * （1）死锁时线程堆栈信息
+ */
+public class DeadLock_Main {
+
+    public static void main(String[] args) {
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+
+        Thread t1 = new Thread(() -> {
+            synchronized (lock1) {
+                Sleeps.seconds(2);
+                synchronized (lock2) {
+                }
+            }
+        }, "t1");
+        Thread t2 = new Thread(() -> {
+            synchronized (lock2) {
+                synchronized (lock1) {
+                }
+            }
+        }, "t2");
+        t1.start();
+        t2.start();
+    }
+}
