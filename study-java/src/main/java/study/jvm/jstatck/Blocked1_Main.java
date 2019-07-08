21f4580de219fdade048108326ca28d42ff15d4f
wangzhenjun <wangzhenjun@okay.cn>
2019-07-08 14:24:40 +0800

    更新代码

---
+++ study-java/src/main/java/study/jvm/jstatck/Blocked1_Main.java
@@ -0,0 +1,25 @@
+package study.jvm.jstatck;
+
+import study.Sleeps;
+
+/**
+ * （1）BLOCKED时线程堆栈信息
+ */
+public class Blocked1_Main {
+
+    public static void main(String[] args) {
+        Object lock = new Object();
+        Thread t1 = new Thread(() -> {
+            synchronized (lock) {
+                Sleeps.deep();
+            }
+        }, "t1");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            synchronized (lock) {
+            }
+        }, "t2");
+        t2.start();
+    }
+}
