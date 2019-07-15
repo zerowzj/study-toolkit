6af313bace02119ea31bfd4207f2152bd2cf7b9d
wangzhenjun <wangzhenjun@okay.cn>
2019-07-15 15:49:31 +0800

    更新代码

--- study-httpclient/src/main/java/study/httpclient/Get_Main.java
+++ study-httpclient/src/main/java/study/httpclient/Get_Main.java
@@ -4,9 +4,11 @@ import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +22,7 @@ public class Get_Main {
 
     public static void main(String[] args) {
         HttpGet httpGet = new HttpGet(URL);
-        //头部
+        //请求头部
         httpGet.setHeader("", "");
         //请求配置
         RequestConfig config = RequestConfig.custom()
@@ -28,6 +30,10 @@ public class Get_Main {
                 .setSocketTimeout(60 * 1000)
                 .build();
         httpGet.setConfig(config);
+        //
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(null);
+        EntityUtils.toString(entity);
+
         HttpClient client = HttpClients.createDefault();
         try {
             HttpResponse response = client.execute(httpGet);
