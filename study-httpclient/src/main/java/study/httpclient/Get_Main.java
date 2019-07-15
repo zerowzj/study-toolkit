04bc9a33546b40f21403574f8b5141ca4f0fffb3
wangzhenjun <wangzhenjun@okay.cn>
2019-07-15 14:40:43 +0800

    更新代码

---
+++ study-httpclient/src/main/java/study/httpclient/Get_Main.java
@@ -0,0 +1,31 @@
+package study.httpclient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class Get_Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Post_Main.class);
+
+    private static final String URL = "http://www.sohu.com";
+
+    public static void main(String[] args) {
+        HttpGet httpGet = new HttpGet(URL);
+        HttpClient client = HttpClients.createDefault();
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine line = response.getStatusLine();
+            LOGGER.info("{}", line);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
