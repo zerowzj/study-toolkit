e6dd7d1a574b5b6f8e1db45c1ada9f707e9146b9
wangzhenjun <wangzhenjun@okay.cn>
2019-07-15 17:05:49 +0800

    更新代码

--- study-httpclient/src/main/java/study/httpclient/entity/StringEntity_Main.java
+++ study-httpclient/src/main/java/study/httpclient/entity/StringEntity_Main.java
@@ -1,8 +1,16 @@
 package study.httpclient.entity;
 
+import org.apache.http.entity.StringEntity;
+
 public class StringEntity_Main {
 
     public static void main(String[] args) {
+
+        try {
+            StringEntity entity = new StringEntity("");
+        } catch (Exception ex) {
+
+        }
 
     }
 }
