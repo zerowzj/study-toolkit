538b55a2f7f6d5bc391edaba78981e5b7b94e085
wangzhenjun <wangzhenjun@okay.cn>
2019-07-16 10:02:40 +0800

    更新代码

--- study-jvm/src/main/java/study/jvm/oom/HeapOOM.java
+++ study-jvm/src/main/java/study/jvm/oom/HeapOOM.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * -Xms5m -Xmx5m -XX:+HeapDumpOnOutOfMemoryError
  */
 public class HeapOOM {
 
