ead72e05edd8a58974916acbf0ab1ca252885704
wangzhenjun <wangzhenjun@okay.cn>
2019-07-17 09:43:44 +0800

    更新代码

--- study-jvm/src/main/java/study/jvm/gc/UseSerialGC_Main.java
+++ study-jvm/src/main/java/study/jvm/gc/UseSerialGC_Main.java
@@ -8,11 +8,9 @@ public class UseSerialGC_Main {
     private static final int _1M = 1024 * 1024;
 
     public static void main(String[] args) {
-
-//        byte[] all1 = new byte[_1M];
-//        byte[] all2 = new byte[_1M];
-        System.out.println("我在活着。。。。");
-        System.gc();
-        System.out.println("我要死了。。。。");
+        System.out.println("我在活着...");
+        byte[] a1 = new byte[_1M];
+        byte[] a2 = new byte[_1M];
+        System.out.println("我要死了...");
     }
 }
