6c56e613bbe27af68ad7da196d08a1fcf390c4fe
wangzhenjun <wangzhenjun@okay.cn>
2019-07-18 09:53:36 +0800

    更新代码

---
+++ study-jvm/src/main/java/study/jvm/gc/Heap_Main.java
@@ -0,0 +1,12 @@
+package study.jvm.gc;
+
+/**
+ * -Xms2M -Xmx2M -XX:+PrintGCDetails -XX:NewRatio=3
+ */
+public class Heap_Main {
+
+    public static void main(String[] args) {
+//        System.out.println("sdfsdfa");
+
+    }
+}
