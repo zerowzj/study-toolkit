5bc43a8901ddd8b30b5f6269a6aaa0cf9868f4e4
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-10-17 14:00:17 +0800

    update

--- study-shell/scripts/jenkins/jenkins.sh
+++ study-shell/scripts/jenkins/jenkins.sh
@@ -2,9 +2,14 @@
 # chkconfig: 2345 90 10
 # description: Jenkins server
 
+JENKINS_PORT=8080
 JEKINS_DIR=/server/jenkins
 JEKINS_WAR=$JEKINS_DIR/jenkins.war
-LOG_FILE=$JEKINS_DIR/logs/stdout.log
+LOG_FILE=$JEKINS_DIR/logs/jenkins.log
+PID_FILE=/var/run/jenkins.pid
+
+# jenkins工作目录，默认位于 /root/.jenkins
+#export JENKINS_HOME=/server/jenkins/
 
 PID=`ps -ef |grep $JEKINS_WAR |grep -v grep |awk '{print $2}'`
 
@@ -13,7 +18,7 @@ case "$1" in
       if [ -z "$PID" ]
       then
           echo "Starting Jenkins..."
-          nohup java -jar $JEKINS_WAR > $LOG_FILE 2>&1 &
+          nohup java -jar $JEKINS_WAR --httpPort=$PORT > $LOG_FILE 2>&1 &
       else
           echo "$PID exists, process is already running or crashed"
       fi
