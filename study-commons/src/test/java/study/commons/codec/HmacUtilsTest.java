6f324b94d14d22b04d4216a0b225f88a3ab81378
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-21 17:02:45 +0800

    update

--- study-commons/src/test/java/study/commons/codec/HmacUtilsTest.java
+++ study-commons/src/test/java/study/commons/codec/HmacUtilsTest.java
@@ -1,5 +1,6 @@
 package study.commons.codec;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.HmacAlgorithms;
 import org.apache.commons.codec.digest.HmacUtils;
@@ -9,7 +10,9 @@ import javax.crypto.Mac;
 
 public class HmacUtilsTest {
 
-    byte[] key = "123213".getBytes();
+    byte[] key = "35b1ab4d984eae5626cdda8d4ff12cf5".getBytes();
+
+    String plaint = "/usercenter/v2/dict/area?nation=中国&province=河南&city=郑州市";
 
     /**
      *
@@ -17,6 +20,7 @@ public class HmacUtilsTest {
     @Test
     public void md5_test() {
         HmacUtils hmacUtils = new HmacUtils(HmacAlgorithms.HMAC_SHA_1, key);
-        System.out.println(hmacUtils.hmacHex("123123"));
+        String b64 = Base64.encodeBase64String(hmacUtils.hmac(plaint));
+        System.out.println(b64);
     }
 }
