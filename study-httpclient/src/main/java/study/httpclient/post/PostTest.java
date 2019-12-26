0cea46e749fce3ef1a21cfff14fb7eaa7b04473f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-26 18:11:28 +0800

    更新代码

--- study-httpclient/src/main/java/study/httpclient/post/PostTest.java
+++ study-httpclient/src/main/java/study/httpclient/post/PostTest.java
@@ -1,38 +1,30 @@
 package study.httpclient.post;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class PostMain {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PostMain.class);
+@Slf4j
+public class PostTest {
 
     private static final String URL = "http://www.sohu.com";
 
     @Test
     public void test() throws Exception {
-        CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(URL);
-        //请求头部
-        httpPost.setHeader("", "");
-        //请求配置
-        RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(60 * 1000)
-                .setSocketTimeout(60 * 1000)
-                .build();
-        httpPost.setConfig(config);
         //请求实体
-        httpPost.setEntity(null);
+        StringEntity entity = new StringEntity("12312312");
+//        httpPost.setEntity(entity);
 
+        CloseableHttpClient client = HttpClients.createDefault();
         HttpResponse response = client.execute(httpPost);
         StatusLine line = response.getStatusLine();
-        LOGGER.info("{}", line);
+        log.info("{}", line);
     }
 }
