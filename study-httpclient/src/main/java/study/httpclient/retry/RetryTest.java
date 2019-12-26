c0f1c65e0b10d81a415c5c43fd53dea96adac7ac
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-26 17:43:34 +0800

    更新代码

--- study-httpclient/src/main/java/study/httpclient/retry/RetryTest.java
+++ study-httpclient/src/main/java/study/httpclient/retry/RetryTest.java
@@ -5,7 +5,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
@@ -15,18 +14,13 @@ public class RetryTest {
     private static final String URL = "https://www.abc.cn/";
 
     @Test
-    public void test() {
-        try {
-            HttpClient client = HttpClients.custom()
-                    .setRetryHandler(new CustomRetryHandler())
-                    .build();
-            HttpGet httpGet = new HttpGet(URL);
-            HttpResponse response = client.execute(httpGet);
-            StatusLine line = response.getStatusLine();
-            log.info("{}", line);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
+    public void test() throws Exception {
+        HttpClient client = HttpClients.custom()
+                .setRetryHandler(new CustomRetryHandler())
+                .build();
+        HttpGet httpGet = new HttpGet(URL);
+        HttpResponse response = client.execute(httpGet);
+        StatusLine line = response.getStatusLine();
+        log.info("{}", line);
     }
 }
