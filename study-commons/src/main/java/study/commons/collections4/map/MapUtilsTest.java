c13ecb37879c5c1c2a3f8ed19561f5deac205efe
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-27 18:29:00 +0800

    更新代码

--- study-commons/src/main/java/study/commons/collections4/map/MapUtilsTest.java
+++ study-commons/src/main/java/study/commons/collections4/map/MapUtilsTest.java
@@ -18,4 +18,50 @@ public class MapUtilsTest {
         String str = MapUtils.getString(map, "i");
         log.info(str);
     }
+
+    @Test
+    public void getMap_test() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("str", "safsafd");
+        map.put("i", 123123);
+        Map obj = MapUtils.getMap(map, "i");
+    }
+
+    @Test
+    public void getObject_test() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("str", "safsafd");
+        map.put("i", 123123);
+        Map obj = MapUtils.getMap(map, "i");
+    }
+
+    @Test
+    public void isEmpty_test() {
+        Map map1 = null;
+        boolean a = MapUtils.isEmpty(map1);
+        log.info("{}", a);
+
+        Map map2 = new HashMap<>();
+        boolean b = MapUtils.isEmpty(map2);
+        log.info("{}", b);
+    }
+    @Test
+    public void isNotEmpty_test() {
+        Map map1 = null;
+        boolean a = MapUtils.isNotEmpty(map1);
+        log.info("{}", a);
+
+        Map map2 = new HashMap<>();
+        boolean b = MapUtils.isNotEmpty(map2);
+        log.info("{}", b);
+    }
+
+    @Test
+    public void emptyIfNull_test() {
+        Map<String, Object> map = null;
+        Map newMap = MapUtils.emptyIfNull(map);
+        newMap.forEach((key, value) -> {
+            log.info("{}={}", key, value);
+        });
+    }
 }
