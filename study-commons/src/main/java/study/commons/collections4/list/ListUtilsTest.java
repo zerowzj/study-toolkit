452765ec18140d0df3cea1ff15dc71afcaaeb65f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-27 18:31:43 +0800

    更新代码

---
+++ study-commons/src/main/java/study/commons/collections4/list/ListUtilsTest.java
@@ -0,0 +1,20 @@
+package study.commons.collections4.list;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
+import org.junit.Test;
+
+import java.util.List;
+
+@Slf4j
+public class ListUtilsTest {
+
+    @Test
+    public void emptyIfNull_test() {
+        List<String> lt = null;
+        List<String> newLt = ListUtils.emptyIfNull(lt);
+        newLt.forEach(str -> {
+            log.info(str);
+        });
+    }
+}
