dae9140a6bddeb17cd5cfcd061bb5eb8006328c2
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-28 10:41:46 +0800

    更新代码

--- study-commons/src/main/java/study/commons/collections4/map/MultiKeyMapTest.java
+++ study-commons/src/main/java/study/commons/collections4/map/MultiKeyMapTest.java
@@ -9,19 +9,13 @@ import org.junit.Test;
 public class MultiKeyMapTest {
 
     @Test
-    public void test() {
+    public void get_test() {
         MultiKeyMap<String, String> map = new MultiKeyMap();
         MultiKey multiKey = new MultiKey("key1", "key2");
-        map.put(multiKey, "123");
-        log.info("key1={}", map.get("key1"));
-        log.info("key2={}", map.get("key3"));
-        log.info("key1, key2={}", map.get(multiKey));
-    }
+        map.put(multiKey, "value");
 
-    @Test
-    public void test1() {
-        MultiKeyMap<String, String> map = new MultiKeyMap();
-        map.put("key1", "key2", "123");
-        log.info("key1={}, key2={}", map.get("key1", "key2"), map.get("key2"));
+        log.info("key1={}", map.get("key1")); //null
+        log.info("key2={}", map.get("key3")); //null
+        log.info("[key1, key2]={}", map.get(multiKey)); //value
     }
 }
