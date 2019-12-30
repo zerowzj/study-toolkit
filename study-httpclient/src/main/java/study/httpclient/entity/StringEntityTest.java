e787beed01abb9608a344f75551fccdc2265a61f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-30 11:04:01 +0800

    更新代码

--- study-httpclient/src/main/java/study/httpclient/entity/StringEntityTest.java
+++ study-httpclient/src/main/java/study/httpclient/entity/StringEntityTest.java
@@ -1,7 +1,10 @@
 package study.httpclient.entity;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
@@ -10,13 +13,20 @@ import java.io.IOException;
 @Slf4j
 public class StringEntityTest {
 
+    private String url = "";
+
     @Test
     public void test() throws IOException {
-        //
-        StringEntity entity = new StringEntity("沙发斯蒂芬", "UTF-8");
-        entity.setContentType("");
-        entity.setContentEncoding("");
-        entity.setChunked(false);
-        log.info(EntityUtils.toString(entity, "UTF-8"));
+        //构造Entity
+        StringEntity strEntity = new StringEntity("沙发斯蒂芬", "UTF-8");
+        strEntity.setContentType("");
+        strEntity.setContentEncoding("");
+        strEntity.setChunked(false);
+        log.info(EntityUtils.toString(strEntity, "UTF-8"));
+        //设置Entity
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(strEntity);
+        CloseableHttpClient client = HttpClients.createDefault();
+        client.execute(httpPost);
     }
 }
--- study-httpclient/src/main/java/study/httpclient/entity/UrlEncodedFormEntityTest.java
+++ study-httpclient/src/main/java/study/httpclient/entity/UrlEncodedFormEntityTest.java
@@ -2,6 +2,9 @@ package study.httpclient.entity;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
@@ -13,17 +16,24 @@ import java.util.List;
 @Slf4j
 public class UrlEncodedFormEntityTest {
 
+    private String url = "";
+
     @Test
     public void test() throws IOException {
-        //
         List<BasicNameValuePair> paramLt = new ArrayList();
         paramLt.add(new BasicNameValuePair("name", "wangzhj"));
         paramLt.add(new BasicNameValuePair("age", "38"));
-        //
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramLt);
-        entity.setContentType("");
-        entity.setContentEncoding("");
-        entity.setChunked(false);
-        log.info(EntityUtils.toString(entity));
+        //构造Entity
+        UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(paramLt);
+//        entity.setContentType("");
+//        entity.setContentEncoding("");
+//        entity.setChunked(false);
+        log.info(EntityUtils.toString(formEntity));
+        //设置Entity
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(formEntity);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        client.execute(httpPost);
     }
 }
