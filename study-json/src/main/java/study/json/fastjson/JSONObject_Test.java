28a9fcea469522b2bf6f57ada8f3379c91990a32
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-06 10:04:26 +0800

    更新代码

--- study-json/src/main/java/study/json/fastjson/JSONObject_Test.java
+++ study-json/src/main/java/study/json/fastjson/JSONObject_Test.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import study.json.Files;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 public class JSONObject_Test {
 
@@ -20,5 +23,34 @@ public class JSONObject_Test {
         String text = Files.read("array.json");
         Object obj = JSONObject.parse(text);
         log.info("{}", obj);
+    }
+
+    @Test
+    public void parse1_test() {
+        Map data = new HashMap<>();
+        data.put("name", "123");
+        Object jsonStr = JSONObject.toJSON(data);
+        log.info("{}", jsonStr);
+    }
+
+    @Test
+    public void isValid_test() {
+        String jsonStr = Files.read("object.json");
+        boolean isJson = JSONObject.isValid(jsonStr);
+        log.info("{}", isJson);
+    }
+
+    @Test
+    public void isValidObject_test() {
+        String jsonStr = Files.read("object.json");
+        boolean isJson = JSONObject.isValidObject(jsonStr);
+        log.info("{}", isJson);
+    }
+
+    @Test
+    public void isValidArray_test() {
+        String jsonStr = Files.read("object.json");
+        boolean isJson = JSONObject.isValidArray(jsonStr);
+        log.info("{}", isJson);
     }
 }
