d1aff78fb09735f447bf3a861a8ada8d37008248
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-09 10:01:37 +0800

    更新代码

--- study-commons/src/main/java/study/commons/io/IOUtilsTest.java
+++ study-commons/src/main/java/study/commons/io/IOUtilsTest.java
@@ -1,5 +1,6 @@
 package study.commons.io;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.FileUtils;
@@ -11,23 +12,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+@Slf4j
 public class IOUtilsTest {
+
+    /**
+     * （★）读文件
+     */
+    @Test
+    public void toString_test() throws Exception {
+        InputStream in = new FileInputStream("d:/234.docx");
+        String str = IOUtils.toString(in, "");
+        log.info(str);
+    }
 
     //toByteArray
     @Test
     public void toByteArray_test() throws Exception {
+
         InputStream in = new FileInputStream("d:/pic.txt");
         byte[] bytes = IOUtils.toByteArray(in);
         byte[] tt = Base64.decodeBase64(bytes);
         FileUtils.writeByteArrayToFile(new File("d:/t.jpg"), tt);
     }
 
-    //toString
-    @Test
-    public void toString_test() throws Exception {
-        InputStream in = new FileInputStream("d:/234.docx");
-        IOUtils.toString(in, "");
-    }
 
     public static String bytesToHexString(byte[] src) {
         StringBuilder stringBuilder = new StringBuilder();
