668b653f7ddf011c4e244877ccc89da76b2bfa0a
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-13 12:55:35 +0800

    更新代码

--- study-serialzition/src/main/java/study/protobuf/DemoTest.java
+++ study-serialzition/src/main/java/study/protobuf/DemoTest.java
@@ -1,6 +1,5 @@
 package study.protobuf;
 
-import com.ynt.ai.activemq.protobuf.PersonModel;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -9,7 +8,7 @@ public class DemoTest {
 
     @Test
     public void test() {
-        PersonModel.Person person = PersonModel.Person.newBuilder()
+        DemoOuterClass.Demo person = DemoOuterClass.Demo.newBuilder()
                 .setId(2)
                 .setName("test")
                 .setEmail("test@163.com")
