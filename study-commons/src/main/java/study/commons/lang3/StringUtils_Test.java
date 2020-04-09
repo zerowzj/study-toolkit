28fbabca794c2b98d4e607aba4beeea3c99f1df6
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-04-09 13:41:59 +0800

    更新代码

--- study-commons/src/main/java/study/commons/lang3/StringUtils_Test.java
+++ study-commons/src/main/java/study/commons/lang3/StringUtils_Test.java
@@ -1,7 +1,6 @@
 package study.commons.lang3;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
--- study-jodatime/src/main/java/study/jodatime/Duration_Test.java
+++ study-jodatime/src/main/java/study/jodatime/Duration_Test.java
@@ -1,10 +1,21 @@
 package study.jodatime;
 
+import org.joda.time.Period;
 import org.junit.Test;
 
 public class Duration_Test {
 
     @Test
     public void test() {
+
+        //2天12小时后过期
+//        Period period = new Period()
+//                .withMillis(10000000);
+//        System.out.println(period.toStandardDays().toPeriod());
+//        System.out.println(period.getDays()+"="+period.getHours());
+
+        String str1 = "2020-01-25";
+        String str2 = "2020-01-15";
+        System.out.println(str1.compareTo(str2));
     }
 }
