13d0ca65848df511b94fa59a30deb55909e8f3d3
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-05-24 12:02:23 +0800

    更新代码

--- study-jodatime/src/main/java/study/jodatime/PeriodTest.java
+++ study-jodatime/src/main/java/study/jodatime/PeriodTest.java
@@ -1,17 +1,19 @@
 package study.jodatime;
 
+import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.joda.time.Period;
 import org.junit.Test;
 
+@Slf4j
 public class PeriodTest {
 
     @Test
     public void test() {
-        DateTime start = new DateTime(2016, 8, 18, 10, 58);
-        DateTime end = new DateTime(2016, 8, 18, 12, 3);
+        DateTime start = new DateTime(2016, 8, 18, 10, 3);
+        DateTime end = new DateTime(2016, 8, 18, 10, 55);
         Period period = new Period(start, end);
-        System.out.println(period.getMonths() + "月" + period.getDays() + "天"
-                + period.getHours() + "小时" + period.getMinutes() + "分");
+        log.info("{}月{}天{}小时{}分{}秒{}毫秒", period.getMonths(), period.getDays(),
+                period.getHours(), period.getMinutes(), period.getSeconds(), period.getMillis());
     }
 }
