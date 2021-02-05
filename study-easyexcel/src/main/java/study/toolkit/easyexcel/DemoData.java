f8620149e8b154d2739dfaae00210dd9c99a9418
wangzhenjun3 <wangzhenjun3@xdf.cn>
2021-02-05 16:52:34 +0800

    更新代码

---
+++ study-easyexcel/src/main/java/study/toolkit/easyexcel/DemoData.java
@@ -0,0 +1,17 @@
+package study.toolkit.easyexcel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class DemoData {
+
+    private String string;
+
+    private Date date;
+
+    private Double doubleData;
+}
--- study-easyexcel/src/main/java/study/toolkit/easyexcel/eeo/TestEEO.java
+++
@@ -1,9 +0,0 @@
-package study.toolkit.easyexcel.eeo;
-
-import com.alibaba.excel.annotation.ExcelProperty;
-
-public class TestEEO {
-
-    @ExcelProperty(index = 1)
-    private String tt ;
-}
