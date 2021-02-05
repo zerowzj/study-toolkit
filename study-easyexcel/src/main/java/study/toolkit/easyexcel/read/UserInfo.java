ef8f56665650e7ed9ed3ad0c0502bf67760e4683
wangzhenjun3 <wangzhenjun3@xdf.cn>
2021-02-05 17:07:43 +0800

    更新代码

---
+++ study-easyexcel/src/main/java/study/toolkit/easyexcel/read/UserInfo.java
@@ -0,0 +1,15 @@
+package study.toolkit.easyexcel.read;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class UserInfo {
+
+    private String email;
+
+    private String userName;
+}
--- study-easyexcel/src/test/java/test/study/toolkit/easyexcel/ReadTest.java
+++ study-easyexcel/src/test/java/test/study/toolkit/easyexcel/ReadTest.java
@@ -2,33 +2,26 @@ package test.study.toolkit.easyexcel;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.read.metadata.ReadSheet;
 import org.junit.Test;
+import study.toolkit.easyexcel.read.UserInfo;
+import study.toolkit.easyexcel.read.UserInfoDataListener;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class ReadTest {
 
     @Test
     public void simpleRead_test() throws Exception {
-        // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
-        // 写法1：
-        String fileName = TestFileUtil.getPath() + "demo" + File.separator + "demo.xlsx";
-        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
-        EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
-
-        // 写法2：
-        fileName = TestFileUtil.getPath() + "demo" + File.separator + "demo.xlsx";
-        ExcelReader excelReader = null;
-        try {
-            excelReader = EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).build();
-            ReadSheet readSheet = EasyExcel.readSheet(0).build();
-            excelReader.read(readSheet);
-        } finally {
-            if (excelReader != null) {
-                // 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
-                excelReader.finish();
-            }
-        }
+        InputStream fis = new FileInputStream("D:\\idea\\study\\study-toolkit\\study-easyexcel\\src\\main\\resources\\excel\\UserInfo.xlsx");
+        AnalysisEventListener listener = new UserInfoDataListener();
+        ExcelReader excelReader = EasyExcel.read(fis, UserInfo.class, listener).build();
+        ReadSheet readSheet = EasyExcel.readSheet(0).build();
+        ReadSheet readSheet2 = EasyExcel.readSheet(1).build();
+        excelReader.read(readSheet);
+        // 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
+        excelReader.finish();
     }
 }
